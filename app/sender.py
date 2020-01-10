ceee1045bbd32e4fcdfe0993903dd8bb842d267b
mauriciomendonca <mauriciomendonca14@gmail.com>
2020-01-10 01:26:30 -0300

    Init python file and sh file

---
+++ app/sender.py
@@ -0,0 +1,11 @@
+from bottle import route, run, request
+
+@route('/', method='POST')
+def send():
+    subject = request.forms.get('subject')
+    message = request.forms.get('message')
+    return f'Message in queue! Subject: {subject} Message: {message}'
+
+
+if __name__ == '__main__':
+    run(host='0.0.0.0', port=8080, debug=True)

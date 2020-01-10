581afe8e87214955563271d43a2a7aa283290eca
mauriciomendonca <mauriciomendonca14@gmail.com>
2020-01-10 17:37:18 -0300

    Function to register message in database

--- app/sender.py
+++ app/sender.py
@@ -1,9 +1,32 @@
+import psycopg2
 from bottle import route, run, request
+
+
+DSN = {
+    'dbname': 'email_sender',
+    'user': 'postgres',
+    'host': 'db'
+}
+INSERT_SQL = 'INSERT INTO email (subject, message) VALUES (%s, %s)'
+
+
+def register_message(subject, message):
+    conn = psycopg2.connect(**DSN)
+    cur = conn.cursor()
+
+    cur.execute(INSERT_SQL, (subject, message))
+    conn.commit()
+
+    cur.close()
+    conn.close()
+
 
 @route('/', method='POST')
 def send():
     subject = request.forms.get('subject')
     message = request.forms.get('message')
+
+    register_message(subject, message)
     return f'Message in queue! Subject: {subject} Message: {message}'
 
 
